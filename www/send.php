a6d20f9d2e9703df3f3b64fa0a17135e0ba09064
Brian Klug <brian.klug@gmail.com>
2016-12-25 19:31:20 -0800

    Update send.php

--- www/send.php
+++ www/send.php
@@ -1,11 +1,9 @@
 <?php
 
-
 $room = $_REQUEST['room'];
 $message = $_REQUEST['message'];
 
-$url = "http://securesocket.io:180/secret_message?room=".urlencode($room)."&message=".urlencode($message);
-
+$url = "https://securesocket.io:1443/secret_message?room=".urlencode($room)."&message=".urlencode($message);
 
 system('curl -s -f -m 3 \''.$url.'\'&');
 
