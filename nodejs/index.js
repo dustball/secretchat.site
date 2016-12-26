97a104160fe49381aaec7a3df3c98f16ac9cf4ab
Brian Klug <brian.klug@gmail.com>
2016-12-25 20:57:39 -0800

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,31 +3,15 @@ var https = require('https');
 
 var app = require('express')();
 
-var privateKey = fs.readFileSync('/root/securesocket.io/domain.key');
-var certificate = fs.readFileSync('/root/securesocket.io/ssl.crt');
-var ca = fs.readFileSync('/root/securesocket.io/int.crt');
+var privateKey = fs.readFileSync('domain.key');
+var certificate = fs.readFileSync('ssl.crt');
+var ca = fs.readFileSync('intermediate.crt');
 
 var server = https.createServer({key:privateKey,cert:certificate,ca:ca}, app);
 
 server.listen(1443);
 
 var io = require('socket.io').listen(server);
-
-app.get('/secret_message', function(req, res){
-	var url = require('url');
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	var room = req.query.room;
-	var message = req.query.message
-
- 	io.sockets.to(""+room).emit('secret_message', {
-     	message: message
- 	});
-
-	res.end()
-	return
-});
-
 
 io.sockets.on('connection', function(socket){
   	console.log("Connection");
@@ -40,11 +24,8 @@ io.sockets.on('connection', function(socket){
 	socket.on('secret_message', function(room,msg){
 		io.sockets.to(""+room).emit('secret_message', {
         	message: msg
-    	});
+    	});		
 		
-  });	 
-
-	
 });
 
 app.listen(180, function () {});
