dcdc821cc1b83e6fef089697b050ecae2e380052
Brian Klug <brian.klug@gmail.com>
2016-12-26 16:37:26 -0800

    Fix GitHub issue button and misc UI improvements

--- www/index.php
+++ www/index.php
@@ -15,11 +15,11 @@ if (!$_SERVER['HTTPS']) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
     html, body {font-family: 'Droid Sans', sans-serif; }
-     #container {padding:2.5em 0; margin:0 3px; margin:0 auto; }
+     #container {padding:2.0em 0; margin:0 3px; margin:0 auto; }
     
       #container {width:480px;}
       @media (max-width: 479px) {
-        #container {width:394px; padding:1.5em 0;}
+        #container {width:394px; padding:1.2em 0;}
       }        
       @media (max-width: 399px) {
         #container {width:312px; padding:0.5em 0;}
@@ -43,7 +43,7 @@ if (!$_SERVER['HTTPS']) {
     <br>
     <form onsubmit="return goroom();">
     <table cellspacing=5>
-      <tr><td align=right>Secret Room Name: </td><td>&nbsp;</td><td><nobr><input type=text style="width:200px" name="room" placeholder="(at least 8 chars)" id="room" maxlength=150 class="ui-corner-all ui-widget" value=""> &nbsp;<span id="len">&nbsp;</span></nobr></td></tr>
+      <tr><td align=right>Secret Room Name: </td><td>&nbsp;</td><td><nobr><input type=password style="width:200px" name="room" placeholder="(at least 8 chars)" id="room" maxlength=150 class="ui-corner-all ui-widget" value=""> &nbsp;<span id="len">&nbsp;</span></nobr></td></tr>
       <tr><td align=right>&nbsp;</td><td>&nbsp;</td><td><button  style="margin-top:.5em" onclick="return goroom();" class="ui-button ui-corner-all ui-widget">Go to Secret Room</button> &nbsp;<span id="len2">&nbsp;</span></td></tr>
     </table>
   
@@ -62,7 +62,7 @@ if (!$_SERVER['HTTPS']) {
 
 <a class="github-button" href="https://github.com/dustball/secretchat.site/fork" data-icon="octicon-repo-forked" data-style="mega" aria-label="Fork dustball/secretchat.site on GitHub">Fork</a>
     <a class="github-button" href="https://github.com/dustball" data-style="mega" data-count-href="/dustball/followers" data-count-api="/users/dustball#followers" data-count-aria-label="# followers on GitHub" aria-label="Follow @dustball on GitHub">Follow @dustball</a>
-    <a class="github-button" href="https://github.com/ntkme/github-buttons/issues" data-icon="octicon-issue-opened" data-style="mega" data-count-api="/repos/ntkme/github-buttons#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue ntkme/github-buttons on GitHub">Issue</a>
+    <a class="github-button" href="https://github.com/dustball/secretchat.site/issues" data-icon="octicon-issue-opened" data-style="mega" data-count-api="/repos/dustball/secretchat.site#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue dustball/secretchat.site on GitHub">Issue</a>
     
     <br>
     <br>
@@ -77,7 +77,7 @@ if (!$_SERVER['HTTPS']) {
 <script>
 
 function goroom() {
-  var passphrase = $("#room").val().toLowerCase();
+  var passphrase = $("#room").val().toLowerCase().trim();
   if (passphrase.length<8) {
     alert("Please enter a room 8 chars or more.");
     ga('send', 'error_short_roomname');
