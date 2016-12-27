522a3938e437e41ae19d4c6737b413c12fb5bccc
Brian Klug <brian.klug@gmail.com>
2016-12-27 13:21:23 -0800

    Nicer 404 page

--- www/404.php
+++ www/404.php
@@ -1,7 +1,51 @@
 <?php
 
   http_response_code(404);
-  print "404 Not Found";
-  exit;
   
-?>
+?><!doctype html>
+<html lang="en">
+<head>
+  <title>404 Not Found</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css" integrity="sha384-12hbHS5VUYVLOm/mmt5zrO3NnhEuXiIwdj3TMACB//xJmJi1lS9lIS89Hwp4E972" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html, body {font-family: 'Droid Sans', sans-serif; }
+     #container {padding:2.0em 0; margin:0 3px; margin:0 auto; }
+    
+      #container {width:480px;}
+      @media (max-width: 479px) {
+        #container {width:394px; padding:1.2em 0;}
+      }        
+      @media (max-width: 399px) {
+        #container {width:312px; padding:0.5em 0;}
+      }
+
+      .gs-badge {display:none !important}
+  </style>
+</head>
+<body>
+  
+  <div id="container">
+  
+  <h1 style="margin-bottom:0">🔒 secretchat.site</h1>
+  
+  <br><br><br><br><br>
+  
+  <h3 style="margin-bottom:0; text-align:center">404 Not Found</h1>
+  
+  <script>
+    // production version will not have google analytics
+    
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-12748037-19', 'auto');
+    ga('send', 'pageview');
+  
+  </script>
+</body>
+</html>
